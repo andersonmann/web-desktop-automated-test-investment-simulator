e9e2f39af00fd64a228279de4749ec3beccf337e
anderson mann <anderson.mann@uhuu.com>
2019-07-10 19:02:10 -0300

    refactor

--- plataforma-digital/src/main/java/page/SimulationPage.java
+++ plataforma-digital/src/main/java/page/SimulationPage.java
@@ -3,8 +3,11 @@
  */
 package page;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
 
 import com.aventstack.extentreports.Status;
 
@@ -19,13 +22,17 @@ public class SimulationPage extends BasePage implements Simulation {
 
 	public void selectProfile(String profile) {
 		try {
-			if (profile.equalsIgnoreCase("pessoa fisica")) {
-				log.getTest().log(Status.INFO, "selecting the profile 'Para você'");
-				click(By.id("//div[text()='Para você']"));
-			}
 			if (profile.equalsIgnoreCase("pessoa juridica")) {
-				log.getTest().log(Status.INFO, "selecting the profile 'Para sua empresa ou agronegócio'");
-				click(By.id("#formInvestimento > div:nth-child(1) > input[type=radio]:nth-child(4)"));
+				List<WebElement> rdBtProfile = driver.findElements(By.name("perfil"));
+				boolean bValue = false;
+				bValue = rdBtProfile.get(0).isSelected();
+				if (bValue == true) {
+					log.getTest().log(Status.INFO, "selecting the profile 'Para sua empresa ou agronegócio'");
+					rdBtProfile.get(1).click();
+				}
+			} else {
+				log.getTest().log(Status.INFO, "selecting the profile 'Para você'");
+				return;
 			}
 		} catch (Exception e) {
 			throw new NoSuchElementException("Option not found: " + e.getMessage());
@@ -48,8 +55,39 @@ public class SimulationPage extends BasePage implements Simulation {
 	}
 
 	public void selectTimeType(String timeType) {
-		// TODO Auto-generated method stub
 
+	}
+
+	public void clickSimuteButton() {
+		log.getTest().log(Status.INFO, "clicking at the button SIMULAR");
+		click(By.className("btn btnAmarelo btnSimular"));
+	}
+
+	public void clickCleanFormkButton() {
+		log.getTest().log(Status.INFO, "clicking at the button Limpar formulário");
+		click(By.className("btn btnAmarelo btnSimular"));
+	}
+
+	public void flowSimulateInvestment(String profile, String initialValue, String monthlyValue, String time,
+			String timeType) {
+		switchTo(0);
+		selectProfile(profile);
+		inputInitialValue(initialValue);
+		inputMonthlylValue(monthlyValue);
+		inputTime(time);
+		selectTimeType(timeType);
+	}
+
+	public void simulateInvestiment(String profile, String initialValue, String monthlyValue, String time,
+			String timeType) {
+		flowSimulateInvestment(profile, initialValue, monthlyValue, time, timeType);
+		clickSimuteButton();
+	}
+
+	public void inputDataAndCleanFields(String profile, String initialValue, String monthlyValue, String time,
+			String timeType) {
+		flowSimulateInvestment(profile, initialValue, monthlyValue, time, timeType);
+		clickCleanFormkButton();
 	}
 
 }
