41a9fcb3485f09444052470dfa556afb010f73c0
anderson mann <anderson.mann@uhuu.com>
2019-07-10 14:50:05 -0300

    created and implemented log and report classes

---
+++ plataforma-digital/src/main/java/util/TestListener.java
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+package util;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import com.aventstack.extentreports.Status;
+
+/**
+ * @author anderson.mann
+ *
+ */
+public class TestListener implements ITestListener {
+	private ExtentTestManager log = new ExtentTestManager();
+	protected static WebDriver driver;
+
+	public void onStart(ITestContext context) {
+		System.out.println("*** Test Suite " + context.getName() + " started ***");
+	}
+
+	public void onFinish(ITestContext context) {
+		System.out.println(("*** Test Suite " + context.getName() + " ending ***"));
+		ExtentTestManager.endTest();
+		ExtentManager.getInstance().flush();
+	}
+
+	public void onTestStart(ITestResult result) {
+		System.out.println(("*** Running test method " + result.getMethod().getMethodName() + "..."));
+		ExtentTestManager.startTest(result.getMethod().getMethodName());
+	}
+
+	public void onTestSuccess(ITestResult result) {
+		System.out.println("*** Executed " + result.getMethod().getMethodName() + " test successfully...");
+		log.getTest().log(Status.PASS, "Test passed");
+	}
+
+	public void onTestFailure(ITestResult result) {
+		System.out.println("*** Test execution " + result.getMethod().getMethodName() + " failed...");
+		log.getTest().log(Status.FAIL, "Test Failed");
+	}
+
+	public String getTestClassName(String testName) {
+		String[] reqTestClassname = testName.split("\\.");
+		int i = reqTestClassname.length - 1;
+		System.out.println("Required Test Name : " + reqTestClassname[i]);
+		return reqTestClassname[i];
+	}
+
+	public void onTestSkipped(ITestResult result) {
+		System.out.println("*** Test " + result.getMethod().getMethodName() + " skipped...");
+		log.getTest().log(Status.SKIP, "Test Skipped");
+	}
+
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		System.out.println("*** Test failed but within percentage % " + result.getMethod().getMethodName());
+	}
+}
