a077188b0cfde982e649dc09dcc17385781d3b56
anderson mann <anderson.mann@uhuu.com>
2019-07-11 13:41:46 -0300

    created test methods

---
+++ plataforma-digital/src/main/java/test/FieldsValidationTest.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package test;
+
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import core.BaseTest;
+import page.SimulationPage;
+
+/**
+ * @author anderson.mann
+ *
+ */
+public class FieldsValidationTest extends BaseTest {
+	private SimulationPage simulation = new SimulationPage();
+
+	@Test
+	public void validateMandatoryFields1() {
+		simulation.validateMandatoryFields();
+		assertEquals("Valor mínimo de 20.00", simulation.getValue(By.id("valorAplicar-error")));
+	}
+
+	@Test
+	public void validateMandatoryFields2() {
+		simulation.validateMandatoryFields();
+		assertEquals("Valor mínimo de 20.00", simulation.getValue(By.id("valorInvestir-error")));
+	}
+
+	@Test
+	public void validateMandatoryFields3() {
+		simulation.validateMandatoryFields();
+		assertEquals("Obrigatório", simulation.getValue(By.id("tempo-error")));
+	}
+
+}
